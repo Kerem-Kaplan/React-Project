a7a5932e8efc10f51ebb361de4ee949a670bb7b1
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-10-28 17:41:19 +0300

    added layout and pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dashboard from './layouts/Dashboard';
+import 'semantic-ui-css/semantic.min.css';
+
+function App() {    //html görünümlü javascript
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
