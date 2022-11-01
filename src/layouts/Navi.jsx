b9e797cd94634a79b9b31220b2d103c023c7b937
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-11-01 21:43:53 +0300

    added cart and detail

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import CartSummary from './CartSummary'
-import { Button, Container, Menu } from 'semantic-ui-react'
+import { Container, Menu } from 'semantic-ui-react'
+import SignedIn from './SignedIn'
+import SignedOut from './SignedOut'
+import { useState } from 'react'
+import{useNavigate} from 'react-router-dom'
 
 export default function Navi() {
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+    
+    const navigate = useNavigate()
+    
+    function handleSignOut() {
+        setIsAuthenticated(false)
+        navigate('/')
+    }
+    function handleSignIn() {
+        setIsAuthenticated(true)
+    }
     return (
         <div>
             <Menu inverted fixed='top'>
                 <Container>
-                    <Menu.Item
-                        name='home'
-                    />
-                    <Menu.Item
-                        name='messages'
-                    />
+                    <Menu.Item name='home' />
+                    <Menu.Item name='messages' />
 
                     <Menu.Menu position='right'>
                         <CartSummary />
-
-                        <Menu.Item>
-                            <Button primary>Sign Up</Button>
-                        </Menu.Item>
+                        {isAuthenticated ? <SignedIn signOut={handleSignOut} /> : <SignedOut signIn={handleSignIn} />}
                     </Menu.Menu>
                 </Container>
 
